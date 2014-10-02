c3cbdd1855bcd473b5609cd15b6d267174fb2b62
Eric Maya <eric.maya.adesis@gmail.com>
2014-10-02 10:54:05 -0500

    se agrega mixin para titular

--- app/scripts/controllers/pasos/init.js
+++ app/scripts/controllers/pasos/init.js
@@ -5,6 +5,41 @@
 
         function setup () {
             console.log('Estamos en el controlador Init.');
+            $scope.reg = {};
+            $scope.reg.letter = /^([a-zA-Z\sÀÈÌÒÙàèìòùÁÉÍÓÚÝáéíóúýÂÊÎÔÛâêîôûÃÑÕãñõÄËÏÖÜäëïöüçÇßØøÅåÆæÞþÐð])+$/;
+            $scope.model = {
+                titular: {
+                    nombre: null,
+                    apellidoPaterno: null,
+                    apellidoMaterno: null,
+                    sexo: null,
+                    rfc: null,
+                    email: null,
+                    telefono: null,
+                    telefono2: null,
+                    beneficiarioPreferente: null
+                }
+            };
+            assignGender();
+        }
+
+        $scope.togglePreferredBeneficiary = function ( _status ) {
+            if ( _status ) {
+                $scope.showPreferredBeneficiary = true;
+            } else {
+                $scope.showPreferredBeneficiary = false;
+            }
+        };
+
+        $scope.calcularRFC = function () {
+            // Se llama al servicio para calcular el rfc.
+            console.log('Se llama al servicio para calcular el rfc.');
+        };
+
+        function assignGender () {
+            if ( ! $scope.model.titular.sexo ) {
+                $scope.model.titular.sexo = 'HOMBRE';
+            }
         }
 
         setup();
